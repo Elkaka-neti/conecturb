1b9e7acf5a8f69dde7da979e17a779dd54ba2d81
unknown <kneves378@gmail.com>
2025-06-16 16:14:21 -0300

    start: Começando o projeto

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    conectUrb começará por aqui 
+    </>
+  )
+}
+
+export default App
