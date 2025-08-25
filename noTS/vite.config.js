26b9018719547346b9e298645fece0ae9459b064
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-08-25 19:01:02 +0000

    [UPDATE] Adicionando configuração de alias para componentes no Vite

--- noTS/vite.config.js
+++ noTS/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@c': "/workspaces/conecturb/noTS/src/components"
+    },
+  }
 })
