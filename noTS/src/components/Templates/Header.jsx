b62eb36e83e6bcc7ac7e5410f862a4e41be41df1
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-09-12 09:23:51 -0300

    update noTS/src/components/Templates/Header.jsx

---
+++ noTS/src/components/Templates/Header.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Menu from '@c/Atoms/Menu'
+import Button from '@c/Atoms/Button'
+
+const Header = ({
+  children,
+  backColor = "white"
+}) => {
+return (
+  <div className="flex justify-between pr-4 pt-2 w-full h-10 relative ">
+   <Menu/>
+   <Button className="bg-orange-400 rounded-xl h-[90%] w-25 text-lg text-white font-bold self-center" href="#login" onClick={() => alert('Login...')}>Login</Button>
+  </div>
+  )
+}
+
+export default Header;
