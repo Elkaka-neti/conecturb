4d27447f152faeb8e70ef5c52bce183270a23cf8
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-09-12 09:14:02 -0300

    update noTS/src/components/Templates/Footer.jsx

---
+++ noTS/src/components/Templates/Footer.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+/*import HomeIcon from "@c/Icons/home.tsx";
+import CategoryIcon from "@c/Icons/category.tsx";
+import PersonIcon from "@c/Icons/person.tsx";
+*/
+const Footer = ({
+  children,
+  backColor = "white"
+}) => {
+return (
+  <div className="flex w-full bg-black/80  backdrop-blur-lg fixed bottom-0 h-[8%] text-white z-80 justify-around text-2xl align-center">
+  hh
+     <HomeIcon />
+     <CategoryIcon/>
+     <PersonIcon/>
+  </div>
+  )
+}
+
+export default Footer;
