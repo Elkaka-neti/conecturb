5d883d5578688b1bf9748ec4bfb6beb8236e0c3f
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-09-22 12:32:36 +0000

    Configurando rotas 2/2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 // Precisamos importar o React para que o JSX funcione
 import React from 'react';
 import Home from './pages/home'
+import Login from './pages/login'
+import { Routes, Route } from 'react-router-dom'
   
 const App = () => {
   return (    
-<Home/>
+<Routes>
+  <Route path="/" element={<Home/>} />
+  <Route path="/login" element={<Login/>} />
+</Routes>
  );
 };
 
