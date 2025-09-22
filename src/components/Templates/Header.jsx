6bf983d37e1eb5f87a12293f3d51aa378c8c64b2
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-09-22 15:28:57 +0000

    Corrigindo redirecionamento do botao. Usando react-router agora.

--- src/components/Templates/Header.jsx
+++ src/components/Templates/Header.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Menu from '../Molecules/Menu'
 import Button from '../Atoms/Global/Button'
-
+import {Link} from 'react-router-dom'
 const Header = ({
   children,
   backColor = "white"
@@ -9,7 +9,9 @@ const Header = ({
 return (
   <div className="flex justify-between pr-4 pt-2 w-full h-10 relative ">
    <Menu/>
-   <Button className="bg-orange-400 rounded-lg h-[90%] w-[30%] text-lg text-white font-bold self-center" href="/login">Login</Button>
+   <Link to="/login">
+     <Button className="bg-orange-400 rounded-lg h-[90%] w-[30%] text-lg text-white font-bold self-center" href="/login">Login</Button>
+   </Link>
   </div>
   )
 }
