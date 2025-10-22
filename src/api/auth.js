db5c7bba94214f58a83ff03131fd0842325f8ee4
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-10-22 22:32:59 +0000

    [UPDATE] Criando logica basica para conexao com api

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,8 +1,15 @@
-import 'axios';
+import axios from 'axios';
 
 export async function registerUser(event) {
   console.log(event);
-  
+  event.preventDefault();
+  const name = (event.target[0].value.toLowerCase().trim() + " " + event.target[1].value.toLowerCase().trim())
+  const email = event.target[2].value;
+  const password = event.target[3].value;
+  //4 --> confirm pass
+  const telefone = event.target[5].value;
+
+  console.log({name, email,  password, telefone})
   try {
     const response = await axios.post("", {
       name,
