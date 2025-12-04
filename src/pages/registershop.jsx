0763d32dca42a28a8c70a514b4b401c03a8f5ad0
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2025-12-04 11:35:18 -0300

    Update src/pages/registershop.jsx via GitMobile

--- src/pages/registershop.jsx
+++ src/pages/registershop.jsx
@@ -7,7 +7,7 @@ const RegisterShop = () => {
         <form className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
             <h2 className="text-2xl font-bold mb-6 text-center">Cadastro de Loja</h2>
             <div className='flex w-[90%]  h-5 justify-center rounded-lg'>
-                
+              hello world i try to use this editor...
             </div>
         </form>
         
